e9f7cf771c75b96511c182a6965e74c205e98e46
Yaroslav <AzisHop777@mail.ru>
2021-04-19 17:51:06 +0300

    centr release done

---
+++ cmake-build-debug/CMakeFiles/async_crypt_dz5_.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/async_crypt_dz5_.dir/main.cpp.o"
+  "async_crypt_dz5_"
+  "async_crypt_dz5_.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/async_crypt_dz5_.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
